6b384a6f368a727dfc94704799c7b05b9a730693
Manoel <mancordosa@gmail.com>
2023-10-21 09:47:38 -0300

    Troquei HashTable por um HashMap por performance

--- src/Grafo.java
+++ src/Grafo.java
@@ -1,37 +1,26 @@
-//import jdk.incubator.vector.IntVector;
-//import jdk.incubator.vector.VectorSpecies;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Hashtable;
-import java.util.Optional;
-import java.util.PrimitiveIterator;
-import java.util.Vector;
+import java.util.*;
 
 public class Grafo {
-    private final @NotNull Hashtable<Integer,Vector<Integer>> vizinhos;
+    private final @NotNull HashMap<@NotNull Integer, @NotNull ArrayList<Integer>> vizinhos;
 
     public Grafo(@NotNull final Node No) {
-        this.vizinhos = new Hashtable<>(1024);
-        this.vizinhos.put(No.getPosAtual(), No.getVizinhos());
-    }
-
-    @Contract(pure = true)
-    public Grafo() {
-        this.vizinhos = new Hashtable<>(1024);
+        this.vizinhos = new HashMap<>();
+        this.vizinhos.put(No.posAtual(), No.vizinhos());
     }
 
     public final void insereGrafo(@NotNull final Node No) {
-        this.vizinhos.putIfAbsent(No.getPosAtual(), No.getVizinhos());
+        this.vizinhos.putIfAbsent(No.posAtual(), No.vizinhos());
     }
 
     @Contract(pure = true)
-    public final @NotNull Hashtable<Integer, Vector<Integer>> getVizinhos() {
-        return vizinhos;
-    }
-
-    @Contract(pure = true)
-    public final Optional<Vector<Integer>> getVizinhosNo(int No) {
+    public final Optional<ArrayList<Integer>> getVizinhosNo(int No) {
         return Optional.ofNullable(this.vizinhos.get(No));
+    }
+
+    public final @NotNull HashMap<@NotNull Integer, @NotNull ArrayList<Integer>> getMap() {
+        return Objects.requireNonNull(vizinhos);
     }
 }
