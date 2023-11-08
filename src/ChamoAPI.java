4adaae4eb11cd1a27ddc1ac6f699ce1c3b58197a
Manoel <mancordosa@gmail.com>
2023-11-08 19:52:13 -0300

    a

--- src/ChamoAPI.java
+++ src/ChamoAPI.java
@@ -14,9 +14,9 @@ public class ChamoAPI {
     private final Gson gson = new Gson();
     private final HttpClient client = HttpClient.newHttpClient();
     private final URI movimento = URI.create("https://gtm.delary.dev/movimentar");
-    private final URI inicio = URI.create("https://gtm.delary.dev/iniciar");
-    private final URI validar = URI.create("https://gtm.delary.dev/validar");
-    private final URI nomeLabirintos = URI.create("https://gtm.delary.dev/labirintos");
+    private final URI inicioUri = URI.create("https://gtm.delary.dev/iniciar");
+    private final URI validarUri = URI.create("https://gtm.delary.dev/validar");
+    private final URI nomesLabirintosUri = URI.create("https://gtm.delary.dev/labirintos");
     private final @NotNull String nomeGrupo = "Um_Grupo";
     private final @NotNull String nomeLabirinto;
 
@@ -35,41 +35,42 @@ public class ChamoAPI {
      */
     @Contract(pure = true)
     final public String getNomes() throws IOException, InterruptedException {
-        final var request = HttpRequest
-                .newBuilder()
-                .GET()
-                .uri(nomeLabirintos)
-                .build();
+        final HttpRequest requisicao = getHttpGetRequest();
 
-        final var response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
+        final HttpResponse<String> resposta = client.send(requisicao, HttpResponse.BodyHandlers.ofString());
 
-        return response.body();
+        return resposta.body();
     }
 
-    final public Node toNode(@NotNull final String json) {
-        return gson.fromJson(json, Node.class);
+    private HttpRequest getHttpGetRequest() {
+        return HttpRequest.newBuilder().GET().uri(nomesLabirintosUri).build();
     }
 
-    final public String inicio() throws IOException, InterruptedException {
-        Inicio inicio = new Inicio(nomeGrupo, nomeLabirinto);
-        final String json = gson.toJson(inicio);
+    final public Node inicio() throws IOException, InterruptedException {
+        final Inicio inicio = new Inicio(nomeGrupo, nomeLabirinto);
+        final String mensagem = gson.toJson(inicio);
+        final String resposta = sendRequest(inicioUri, mensagem);
 
-        return sendRequest(this.inicio, json);
+        return gson.fromJson(resposta, Node.class);
     }
 
     /**
-     * @param No Proximo nó a ser enviado para a API
+     * @param No Próximo nó a ser enviado para a API
      * @return String com o json bruto retornado pela API
      * @throws IOException          Erro de conexão
      * @throws InterruptedException ^C (Processo cancelado)
      */
     @Contract(pure = true)
-    final public String proxMovimento(final int No) throws IOException, InterruptedException {
-        Movimento movimento = new Movimento(this.nomeGrupo, this.nomeLabirinto, No);
-        final String json = gson.toJson(movimento);
+    final public Node proxMovimento(final int No) throws IOException, InterruptedException {
+        final String mensagem =
+                gson.toJson(new Movimento(this.nomeGrupo, this.nomeLabirinto, No));
+        final String resposta = sendRequest(this.movimento, mensagem);
 
-        return sendRequest(this.movimento, json);
+        return toNode(resposta);
+    }
+
+    private Node toNode(@NotNull String json) {
+        return gson.fromJson(json, Node.class);
     }
 
     /**
@@ -80,47 +81,63 @@ public class ChamoAPI {
      */
     // Not tested
     @Contract(pure = true)
-    final public @NotNull CaminhoValido fim(final ArrayList<Integer> caminho) throws IOException, InterruptedException {
-        ValidaCaminho validaCaminho = new ValidaCaminho(this.nomeGrupo, this.nomeLabirinto, caminho);
+    final public @NotNull CaminhoValidado fim(final ArrayList<Integer> caminho)
+            throws IOException, InterruptedException {
+        CaminhoParaValidar validaCaminho =
+                new CaminhoParaValidar(this.nomeGrupo, this.nomeLabirinto, caminho);
 
-        final String json = gson.toJson(validaCaminho);
+        final String mensagem = gson.toJson(validaCaminho);
 
-        final String retorno = sendRequest(this.validar, json);
+        final String resposta = sendRequest(this.validarUri, mensagem);
 
-        return gson.fromJson(retorno, CaminhoValido.class);
+        return gson.fromJson(resposta, CaminhoValidado.class);
     }
 
     /**
-     * @param uri
-     * @param json
+     * @param uri  Para onde a mensagem será enviada
+     * @param json Mensagem que será enviada.
      * @return Caso a conexão retorne um código de erro, todo o programa irá parar.
      * Caso o processo retorne normalmente, será retornado o json gerado pela conexão.
-     * @throws IOException
-     * @throws InterruptedException
+     * @throws IOException          Problemas com a conexão
+     * @throws InterruptedException Processo foi interrompido durante a conexão
      */
     @Contract(pure = true)
-    private String sendRequest(final URI uri, final String json) throws IOException, InterruptedException {
-        HttpRequest request = HttpRequest
+    private String sendRequest(final @NotNull URI uri, final @NotNull String json)
+            throws IOException, InterruptedException {
+
+        final HttpRequest mensagem = getHttpPostRequest(uri, json);
+        final HttpResponse<String> resposta =
+                client.send(mensagem, HttpResponse.BodyHandlers.ofString());
+
+        validaRetorno(resposta);
+
+        return resposta.body();
+    }
+
+    private HttpRequest getHttpPostRequest(@NotNull URI uri, @NotNull String json) {
+        return HttpRequest
                 .newBuilder()
                 .uri(uri)
                 .headers("Content-Type", "application/json")
                 .POST(HttpRequest.BodyPublishers.ofString(json))
                 .build();
-
-        validaRetorno(request);
-
-        return client.send(request, HttpResponse.BodyHandlers.ofString()).body();
     }
 
-
     /**
-     * @param request
-     * @throws IOException
-     * @throws InterruptedException Verifica que a conexão retorna algo válido, senão, para de executar o programa.
+     * A API tem apenas dois valores, sucesso e erro.
+     * Caso encontremos o estado de erro, crashar o programa
+     *
+     * @param resposta Comunicação com a API que precisa ser validada
      */
-    private void validaRetorno(HttpRequest request) throws IOException, InterruptedException {
-        if (client.send(request, HttpResponse.BodyHandlers.ofString()).statusCode() == 422) {
-            System.err.println("Comunicação.Movimento inválido");
+    private <T> void validaRetorno(final @NotNull HttpResponse<T> resposta) {
+        /**
+         * A API, no momento, possui apenas dois códigos de resposta, 200 e 422.
+         * O erro é gerado caso haja um erro de formatação no json _ou_ um movimento inválido ocorre.
+         * Para facilitar o processo de debug, caso ocorra um erro, o programa irá crashar instantaneamente.
+         */
+        final int ERROR_CODE = 422;
+        if (resposta.statusCode() == ERROR_CODE) {
+            System.err.println("Movimento inválido realizado");
             System.err.println("Parando execução");
             System.exit(-1);
         }
