ab2ab7acf9a8e7d2317bdb56040e4529a4516734
Manoel <mancordosa@gmail.com>
2023-11-08 10:13:21 -0300

    Removido

--- src/ValidaCaminho.java
+++
@@ -1,7 +0,0 @@
-import org.jetbrains.annotations.NotNull;
-
-import java.util.ArrayList;
-
-public record ValidaCaminho(@NotNull String id, @NotNull String nome, @NotNull ArrayList<Integer> caminho) {
-
-}
