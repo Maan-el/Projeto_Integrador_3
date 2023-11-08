04580ca7e34a1e4fb852b3b4da7d59acf85349b1
Manoel <mancordosa@gmail.com>
2023-11-08 19:47:17 -0300

    Erros foram cometidos

--- src/comunicacao/Node.java
+++ src/comunicacao/Node.java
@@ -6,7 +6,7 @@ import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
 
 public record Node(@SerializedName("pos_atual") @NotNull Integer posAtual,
-                   @SerializedName("Comunicação.Inicio") boolean inicio,
+                   @SerializedName("inicio") boolean inicio,
                    @SerializedName("final") boolean fim,
                    @SerializedName("movimentos") @NotNull ArrayList<Integer> vizinhos) {
 }
