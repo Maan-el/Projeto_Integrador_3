5f5a4ccb61893173f2e49cbf5a1499600915a0a4
Manoel <mancordosa@gmail.com>
2023-11-09 19:05:53 -0300

    Valores necessários para gerar o grafo

--- src/Grafo.java
+++ src/Grafo.java
@@ -1,2 +1,13 @@
 public class Grafo {
+import comunicacao.Node;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public record Grafo(@NotNull Integer inicio,
+                    @NotNull HashSet<Integer> saidas,
+                    @NotNull HashMap<@NotNull Integer, @NotNull ArrayList<@NotNull Integer>> listaAdjacencia) {
+    public void add(@NotNull final Node node) {
 }
