838558e4a1872d44aa06a1bde81253a9919bb924
Manoel <mancordosa@gmail.com>
2023-11-09 20:17:04 -0300

    Grafo reinserido, para testes

--- src/DFS.java
+++ src/DFS.java
@@ -2,16 +2,15 @@ import comunicacao.Node;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 public class DFS {
     @NotNull
     final ChamoAPI API;
     @NotNull
     final HashSet<Integer> visitados;
+
+    Grafo grafo;
 
     public DFS() {
         visitados = new HashSet<>();
@@ -20,6 +19,9 @@ public class DFS {
 
     final public ArrayList<Integer> inicio() throws IOException, InterruptedException {
         Node node = API.inicio();
+
+        grafo = new Grafo(node.posAtual(), new HashSet<>(), new HashMap<>());
+        grafo.add(node);
 
         visitados.add(node.posAtual());
         final var caminhoReverso = dfs(node.posAtual(), node.vizinhos()).orElse(null);
@@ -36,23 +38,33 @@ public class DFS {
                                              @NotNull final ArrayList<Integer> vizinhos) throws IOException, InterruptedException {
 
         for (var item : vizinhos) {
-            if (visitados.add(item)) {
-                Node node = API.proxMovimento(item);
-
-                if (node.fim()) return Optional.of(new ArrayList<>(List.of(node.posAtual(), raiz)));
-
-                Optional<ArrayList<Integer>> retorno = dfs(node.posAtual(), node.vizinhos());
-
-                if (retorno.isPresent()) {
-                    retorno.get().add(raiz);
-                    return retorno;
-                }
-                //noinspection ResultOfMethodCallIgnored
-                API.proxMovimento(raiz);
-            }
+            Optional<ArrayList<Integer>> node = getIntegers(raiz, item);
+            if (node.isPresent()) return node;
         }
         return Optional.empty();
     }
 
+    private Optional<ArrayList<Integer>> getIntegers(@NotNull Integer raiz, @NotNull Integer item) throws IOException, InterruptedException {
+        if (visitados.add(item)) {
+            Node node = API.proxMovimento(item);
+
+            if (node.fim()) return Optional.of(new ArrayList<>(List.of(node.posAtual(), raiz)));
+
+            Optional<ArrayList<Integer>> retorno = dfs(node.posAtual(), node.vizinhos());
+
+            if (retorno.isPresent()) {
+                retorno.get().add(raiz);
+                return retorno;
+            }
+
+            //noinspection ResultOfMethodCallIgnored
+            API.proxMovimento(raiz);
+        }
+        return Optional.empty();
+    }
+
+    public final Grafo getGrafo() {
+        return grafo;
+    }
 }
 
