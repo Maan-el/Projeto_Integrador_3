5e8e964a0809537c8b7f46afcee31772ded9746d
Manoel <mancordosa@gmail.com>
2023-11-10 20:01:15 -0300

    Criada separação entre final com validação e final sem validação

--- src/Main.java
+++ src/Main.java
@@ -19,7 +19,7 @@ public class Main {
         System.out.println(movimentos);
 
         try {
-            api.fim(movimentos);
+            api.fim_debug(movimentos);
         } catch (IOException | InterruptedException e) {
             System.err.println(e.getMessage());
         }
