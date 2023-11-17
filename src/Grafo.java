c1fdad83575476f4d249481d7f928b8fa8781965
Manoel <mancordosa@gmail.com>
2023-11-17 18:16:13 -0300

    Carcaça

--- src/Grafo.java
+++ src/Grafo.java
@@ -4,10 +4,15 @@ import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Optional;
 
 public record Grafo(@NotNull Integer inicio,
                     @NotNull HashSet<Integer> saidas,
                     @NotNull HashMap<@NotNull Integer, @NotNull ArrayList<@NotNull Integer>> listaAdjacencia) {
+    public Optional<ArrayList<Integer>> get(@NotNull final Integer posicao) {
+        return Optional.ofNullable(listaAdjacencia.get(posicao));
+    }
+
     public void add(@NotNull final Node node) {
         listaAdjacencia.putIfAbsent(node.posAtual(), node.vizinhos());
 
