b54aab5e9fa999b9712fc7ca5d10bcbc53fd4a22
manoel <mancordosa@gmail.com>
2023-12-03 11:34:09 -0300

    Funções para transformar json em classes

--- src/Parser.java
+++ src/Parser.java
@@ -1,7 +1,5 @@
 import com.google.gson.Gson;
-import comunicacao.Inicio;
-import comunicacao.Movimento;
-import comunicacao.Node;
+import comunicacao.*;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
@@ -12,5 +10,30 @@ public class Parser {
 
     public Parser() {
         this.gson = new Gson();
+    }
+
+    @Contract(pure = true)
+    public final @NotNull Function<String, Node> toNode() {
+        return (json) -> gson.fromJson(json, Node.class);
+    }
+
+    @Contract(pure = true)
+    public final @NotNull Function<String, Inicio> toInicio() {
+        return (json) -> gson.fromJson(json, Inicio.class);
+    }
+
+    @Contract(pure = true)
+    public final @NotNull Function<String, Movimento> toMovimento() {
+        return (json) -> gson.fromJson(json, Movimento.class);
+    }
+
+    @Contract(pure = true)
+    public final @NotNull Function<String, CaminhoParaValidar> toCaminhoParaValidar() {
+        return (json) -> gson.fromJson(json, CaminhoParaValidar.class);
+    }
+
+    @Contract(pure = true)
+    public final @NotNull Function<String, CaminhoValidado> toCaminhoValidado() {
+        return (json) -> gson.fromJson(json, CaminhoValidado.class);
     }
 }
